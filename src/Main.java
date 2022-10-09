85b03565ddd5d7f754569a1981f3f3db6dc7b0a8
poketuk <poketuk@yandex.ru>
2022-10-10 00:10:58 +0300

    Выполнено Домашнее задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    }
+
+    public static void checkLeapYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(year + " is leap year");
+        } else {
+            System.out.println(year + " is not leap year");
+        }
+    }
+
+    public static void getDeviceLink(int osType, int manufacturingYear) {
+        if (osType != 1 && osType != 0) {
+            throw new IllegalArgumentException("Wrong OS type");
+        }
+        int currentYear = LocalDate.now().getYear();
+        if (manufacturingYear > currentYear) {
+            throw new IllegalArgumentException("Wrong manufacturing year");
+        }
+        String osString = osType == 0 ? "iOS" : "Android";
+        String versionString = manufacturingYear > 2015 ? "полную" : "облегченную";
+        System.out.printf("Установите %s версию для %s по ссылке%n", versionString, osString);
+    }
+
+    public  static  int calculateDeliveryDays(int deliveryDistance) {
+        if (deliveryDistance <= 20) {
+            return 1;
+        } else {
+            return (int) Math.round((double) deliveryDistance / 40) + 1;
+        }
     }
 }
